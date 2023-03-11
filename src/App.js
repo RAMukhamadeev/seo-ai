748bd99230603feda38d45a96901a46f41c32a35
Ruslan Mukhamadeev <ruslan910425@gmail.com>
2023-03-11 03:01:50 +0100

    add all tones

--- src/App.js
+++ src/App.js
@@ -3,62 +3,59 @@ import {useState} from "react";
 import axios from "axios";
 
 const tones = [
-    {
-        "label": "Appreciative",
-        "value": "Appreciative"
-    },
-    {
-        "label": "Assertive",
-        "value": "Assertive"
-    },
-    {
-        "label": "Awestruck",
-        "value": "Awestruck"
-    },
-    {
-        "label": "Candid",
-        "value": "Candid"
-    },
-    {
-        "label": "Casual",
-        "value": "Casual"
-    },
-    {
-        "label": "Cautionary",
-        "value": "Cautionary"
-    },
-    {
-        "label": "Compassionate",
-        "value": "Compassionate"
-    },
-    {
-        "label": "Convincing",
-        "value": "Convincing"
-    },
-    {
-        "label": "Critical",
-        "value": "Critical"
-    },
-    {
-        "label": "Earnest",
-        "value": "Earnest"
-    },
-    {
-        "label": "Enthusiastic",
-        "value": "Enthusiastic"
-    },
-    {
-        "label": "Formal",
-        "value": "Formal"
-    },
-    {
-        "label": "Funny",
-        "value": "Funny"
-    },
-    {
-        "label": "Humble",
-        "value": "Humble"
-    }
+    {"value": "Appreciative", "label": "Appreciative"},
+    {"value": "Assertive", "label": "Assertive"},
+    {"value": "Authoritative", "label": "Authoritative"},
+    {"value": "Awestruck", "label": "Awestruck"},
+    {"value": "Bold", "label": "Bold"},
+    {"value": "Calming", "label": "Calming"},
+    {"value": "Candid", "label": "Candid"},
+    {"value": "Caring", "label": "Caring"},
+    {"value": "Casual", "label": "Casual"},
+    {"value": "Cautionary", "label": "Cautionary"},
+    {"value": "Compassionate", "label": "Compassionate"},
+    {"value": "Confident", "label": "Confident"},
+    {"value": "Contemplative", "label": "Contemplative"},
+    {"value": "Convincing", "label": "Convincing"},
+    {"value": "Critical", "label": "Critical"},
+    {"value": "Dramatic", "label": "Dramatic"},
+    {"value": "Earnest", "label": "Earnest"},
+    {"value": "Edgy", "label": "Edgy"},
+    {"value": "Emotional", "label": "Emotional"},
+    {"value": "Energetic", "label": "Energetic"},
+    {"value": "Enthusiastic", "label": "Enthusiastic"},
+    {"value": "Exaggerated", "label": "Exaggerated"},
+    {"value": "Formal", "label": "Formal"},
+    {"value": "Friendly", "label": "Friendly"},
+    {"value": "Funny", "label": "Funny"},
+    {"value": "Humble", "label": "Humble"},
+    {"value": "Humorous", "label": "Humorous"},
+    {"value": "Informative", "label": "Informative"},
+    {"value": "Innovative", "label": "Innovative"},
+    {"value": "Inspirational", "label": "Inspirational"},
+    {"value": "Intellectual", "label": "Intellectual"},
+    {"value": "Intense", "label": "Intense"},
+    {"value": "Intriguing", "label": "Intriguing"},
+    {"value": "Irreverent", "label": "Irreverent"},
+    {"value": "Luxurious", "label": "Luxurious"},
+    {"value": "Modern", "label": "Modern"},
+    {"value": "Motivational", "label": "Motivational"},
+    {"value": "Nostalgic", "label": "Nostalgic"},
+    {"value": "Outrageous", "label": "Outrageous"},
+    {"value": "Patriotic", "label": "Patriotic"},
+    {"value": "Playful", "label": "Playful"},
+    {"value": "Powerful", "label": "Powerful"},
+    {"value": "Quirky", "label": "Quirky"},
+    {"value": "Reassuring", "label": "Reassuring"},
+    {"value": "Romantic", "label": "Romantic"},
+    {"value": "Sarcastic", "label": "Sarcastic"},
+    {"value": "Sophisticated", "label": "Sophisticated"},
+    {"value": "Straightforward", "label": "Straightforward"},
+    {"value": "Sympathetic", "label": "Sympathetic"},
+    {"value": "Thought-provoking", "label": "Thought-provoking"},
+    {"value": "Trustworthy", "label": "Trustworthy"},
+    {"value": "Upbeat", "label": "Upbeat"},
+    {"value": "Witty", "label": "Witty"}
 ];
 const frameworks = [
     {
@@ -278,15 +275,9 @@ const App = () => {
             <div className="container py-3">
                 <header>
                     <div className="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
-                        <a href="/" className="d-flex align-items-center text-dark text-decoration-none">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="40" height="32" className="me-2"
-                                 viewBox="0 0 118 94" role="img"><title>Bootstrap</title>
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                      d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"
-                                      fill="currentColor"></path>
-                            </svg>
-                            <span className="fs-4">Pricing example</span>
-                        </a>
+                        <span className="d-flex align-items-center text-dark text-decoration-none">
+                            <span className="fs-4">AI Advertisement Text Generator</span>
+                        </span>
 
                         <nav className="d-inline-flex mt-2 mt-md-0 ms-md-auto">
                             <a className="me-3 py-2 text-dark text-decoration-none" href="#">Features</a>
